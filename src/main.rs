4b137d05885223dc18540cb14b7f0819a0bf94df
Jason Jordan <JasonAJordan0@gmail.com>
2021-05-26 12:34:58 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// use bevy::{
+//     //core::FixedTimestep,
+//     prelude::*,
+//     render::pass::ClearColor,
+//     sprite::collide_aabb::{collide, Collision},
+// };
+
+
+fn main() {
+    // App::build()
+    //     //game setup
+    //     .add_plugins(DefaultPlugins)
+    //     .insert_resource(WindowDescriptor {
+    //         title: "Space Invaders".to_string(),
+    //         vsync: true,
+    //         ..Default::default()
+    //     })
+    //     .run();
+    println!("hi")
+}
